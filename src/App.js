903d49cffb31291e7a6a001734e95d59856dbadc
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2023-01-15 15:25:58 +0100

    display X when square is clicked+update game state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 function App() {
   const squares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  // const [setSquaresState, currentSquaresState] = useState(squares);
+
+  function show(event) {
+    let playedSquareId = event.target.id;
+    console.log(playedSquareId);
+    let squarePlayed = document.getElementById(playedSquareId);
+    squarePlayed.innerHTML = "X";
+    squares[playedSquareId] = "X";
+    console.log(squares);
+  }
 
   return (
     <div className="game">
       <div className="frame">
         {squares.map((square, index) => (
-          <img
-            key={index}
-            style={{ backgroundColor: "green" }}
-            alt={square}
-          ></img>
+          <div onClick={show} key={index} id={index}></div>
         ))}
       </div>
     </div>
