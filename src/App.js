c4dada62f5e57a97c3065743ee9e51df6a5d031a
Gregory <85247790+Gregory-Bagramyan@users.noreply.github.com>
2023-01-15 16:00:26 +0100

    added robot turn

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import { useState } from "react";
 
 function App() {
   const squares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const availableSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   // const [setSquaresState, currentSquaresState] = useState(squares);
 
   function show(event) {
     let playedSquareId = event.target.id;
-    console.log(playedSquareId);
     let squarePlayed = document.getElementById(playedSquareId);
     squarePlayed.innerHTML = "X";
     squares[playedSquareId] = "X";
+    let notAvailableIndex = availableSquares.indexOf(Number(playedSquareId));
+    availableSquares.splice(notAvailableIndex, 1);
     console.log(squares);
+    console.log(availableSquares);
+    robotPlay();
   }
+
+  const robotPlay = () => {
+    let robotChoice =
+      availableSquares[Math.floor(Math.random() * availableSquares.length)];
+    console.log(robotChoice);
+    let squareChosenByRobot = document.getElementById(robotChoice);
+    squareChosenByRobot.innerHTML = "O";
+    squares[robotChoice] = "O";
+    let notAvailableIndex = availableSquares.indexOf(Number(robotChoice));
+    availableSquares.splice(notAvailableIndex, 1);
+    console.log(squares);
+    console.log(availableSquares);
+  };
 
   return (
     <div className="game">
